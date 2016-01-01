75c561e9143becc4aa5e0e20847e44a656d6e769
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2016-01-01 12:40:05 +0800

    Initial project structure.

---
+++ app/src/main/java/com/example/android/sunshine/fragments/MainFragment.java
@@ -0,0 +1,24 @@
+package com.example.android.sunshine.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.sunshine.R;
+
+/**
+ * A placeholder fragment containing a simple view for the MainActivity.
+ */
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
