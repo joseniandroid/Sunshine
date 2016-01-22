2ddf99e166420cf4752f41c67affcaf9a65a6127
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2016-01-23 03:23:54 +0800

    3.06 Inflated a settings menu to DetailActivity.

--- app/src/main/java/com/example/android/sunshine/activities/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/activities/DetailActivity.java
@@ -3,6 +3,8 @@ package com.example.android.sunshine.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.android.sunshine.R;
 
@@ -18,4 +20,20 @@ public class DetailActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
