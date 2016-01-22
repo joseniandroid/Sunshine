d8e6b97f0076df6fce876bf5398c337a14424515
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2016-01-23 03:04:38 +0800

    3.03 Launch DetailActivity by initiating an explicit intent.

--- app/src/main/java/com/example/android/sunshine/fragments/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/fragments/ForecastFragment.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.sunshine.fragments;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -31,10 +32,10 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.example.android.sunshine.BuildConfig;
 import com.example.android.sunshine.R;
+import com.example.android.sunshine.activities.DetailActivity;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -127,7 +128,8 @@ public class ForecastFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String forecast = mForecastAdapter.getItem(position);
-                Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), DetailActivity.class);
+                startActivity(intent);
             }
         });
     }
